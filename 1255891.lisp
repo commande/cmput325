505eb5843d5a82f67b41f0212e8da71dba465e0f
Brett Commandeur <commande@ualberta.ca>
2017-01-31 18:17:30 -0700

    Add files via upload

---
+++ 1255891.lisp
@@ -0,0 +1,64 @@
+;;;; 1255891.lisp
+;;;; Deliverable for Assignment 1
+
+;;;; Brett Commandeur
+;;;; 1255891
+;;;; CMPUT 325 Wi17 - NON-PROCEDURAL PROG LANGUAGES Combined LAB LEC Wi17
+
+#|--- TODO: Remove this comment
+
+Acceptable built-in functions: 
+
+(atom x)
+(null x)
+(eq x y)
+(equal x y)
+(numberp x)
+(append x y)
+(car x)
+- (cadr ...)
+(cdr x)
+- (cdaar ...)
+(cons x y) 
+(if x y z)
+(cond ... ) 
+(let ((x y) (u v)) z)
+(let* ((x y) (u v)) z)
+(defun ...)
+(quote x) and its short form 'x
+(list x1 x2 ...)
+(print ...)
+(sort L fun) % this is useful for the last problem
+(copy-list L) % useful in conjunction with sort 
+- http://clhs.lisp.se/Body/f_cp_lis.htm#copy-list
+- (sort (copy-list L) sort-predicate)
+
+(+ x y)
+(- x y)
+(* x y)
+(/ x y)
+(< x y)
+(> x y)
+(= x y)
+(<= x y)
+(>= x y)
+(and x y)
+(or x y)
+(not x)
+
+|#
+
+;;; QUESTION 1
+
+;; documentation
+(defun xmember (X Y)
+	" This "
+)
+
+;; documentation
+(defun q1_helper_1 (...)
+...
+)
+
+;; QUESTION 2
+
