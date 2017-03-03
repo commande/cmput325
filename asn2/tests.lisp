1778626c01b7cd32344ce92fb0195d5cefeef07f
“Brett <bcommandeur@gmail.com>
2017-03-02 19:51:39 -0700

    setup marked tests

--- asn2/tests.lisp
+++ asn2/tests.lisp
@@ -34,6 +34,33 @@
 ;;; Tests
 (deftest test-primitives ()
   (check
+    ;;; Basic
+    (equal (fl-interp '(+ 10 5) nil) '15)
+    (equal (fl-interp '(- 12 8) nil) '4)
+    (equal (fl-interp '(* 5 9) nil) '45)
+    (equal (fl-interp '(> 2 3) nil) 'nil)
+    (equal (fl-interp '(< 1 131) nil) 't)
+    (equal (fl-interp '(= 88 88) nil) 't)
+    (equal (fl-interp '(and nil t) nil) 'nil)
+    (equal (fl-interp '(or t nil) nil) 't)
+    (equal (fl-interp '(not t) nil) 'nil)
+    (equal (fl-interp '(isnumber 354) nil) 't)
+    (equal (fl-interp '(equal (3 4 1) (3 4 1)) nil) 't)
+    (equal (fl-interp '(if nil 2 3) nil) '3)
+    (equal (fl-interp '(null ()) nil) 't)
+    (equal (fl-interp '(atom (3)) nil) 'nil)
+    (equal (fl-interp '(eq x x) nil) 't)
+    (equal (fl-interp '(first (8 5 16)) nil) '8)
+    (equal (fl-interp '(rest (8 5 16)) nil) '(5 16))
+    (equal (fl-interp '(cons 6 3) nil) '(6 . 3))
+
+    ;;; Complex
+    (equal (fl-interp '(+ (* 2 2) (* 2 (- (+ 2 (+ 1 (- 7 4))) 2))) nil) '12)
+    (equal (fl-interp '(and (> (+ 3 2) (- 4 2)) (or (< 3 (* 2 2))) (not (= 3 2))) nil) 't)
+    (equal (fl-interp '(or (= 5 (- 4 2)) (and (not (> 2 2)) (< 3 2))) nil) 'nil)
+    (equal (fl-interp '(if (not (null (first (a c e)))) (if (isnumber (first (a c e))) (first (a c e)) (cons (a c e) d)) (rest (a c e))) nil) '((a c e) . d))
+
+    ;;; Custom
     ;; First and Rest
     (equal (fl-interp '(first (1 2 3)) nil) '1)
     (equal (fl-interp '(rest (p 1 2 (3))) nil) '(1 2 (3)))
